b96c5c6ae4de3d7401c67074ab3a547b4f6e145e
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-05 10:03:05 +0000

    Set up PostgreSQL connection, update settings, and apply migrations

--- mystar/settings.py
+++ mystar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('envy.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8bh8)3pu@2f_v3n=+3a6n(h0w1xn5*$r^b@7esf)kc%o7z)v%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-milosson-travelblog-tvh83p9sgui.ws-eu115.gitpod.io', '.herokuapp.com' ]
 
@@ -74,11 +78,16 @@ WSGI_APPLICATION = 'mystar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : dj_database_url.parse("postgres://u3mpwxwhyjd:RekaesP3ADEo@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/steam_prism_ranch_984061")
+
 }
 
 
