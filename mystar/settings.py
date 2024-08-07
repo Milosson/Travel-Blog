9722a578bab9c611c37c513a041c7cd34a5b8553
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-07 10:41:24 +0000

    Add Post model to blog app with fields and migration

--- mystar/settings.py
+++ mystar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8bh8)3pu@2f_v3n=+3a6n(h0w1xn5*$r^b@7esf)kc%o7z)v%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-milosson-travelblog-tvh83p9sgui.ws-eu115.gitpod.io', '.herokuapp.com' ]
 
