a88caeaa04f49f5acc7d5b6b85e3bc8ea2eec7e1
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-09 17:54:43 +0000

    Added description to models.py, and debug set to true in settings.py

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,23 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
+    """
+    A model representing a blog post.
+
+    Attributes:
+        title (str): The title of the post.
+        slug (str): A slugified version of the title, used for URLs.
+        author (User): The author of the post, linked to the User model.
+        content (str): The main content of the post.
+        created_on (datetime): The date and time the post was created.
+        status (int): The publication status of the post (0 for Draft, 1 for Published).
+        excerpt (str): An optional short excerpt from the post.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
