58fce6339b804db09b5f622eb98d74714ca27608
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-21 19:41:30 +0000

    Add 'about' app and include it in INSTALLED_APPS

--- mystar/settings.py
+++ mystar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-8bh8)3pu@2f_v3n=+3a6n(h0w1xn5*$r^b@7esf)kc%o7z)v%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [    
     '8000-milosson-travelblog-5vyjtvxthhd.ws-eu115.gitpod.io',    
@@ -49,8 +49,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_summernote',
     'blog',
+    'django_summernote',
+    'about',
+
 ]
 
 MIDDLEWARE = [
