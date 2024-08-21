b63d66f725ae27696931a0114378860667688b8d
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-21 22:26:56 +0000

    Refactor static file settings and tidy up settings.py for better clarity

--- mystar/settings.py
+++ mystar/settings.py
@@ -10,16 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 import dj_database_url
-if os.path.isfile('envy.py'):
+
+# Load environment variables if env.py is present
+if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -30,8 +31,8 @@ SECRET_KEY = 'django-insecure-8bh8)3pu@2f_v3n=+3a6n(h0w1xn5*$r^b@7esf)kc%o7z)v%h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [    
-    '8000-milosson-travelblog-5vyjtvxthhd.ws-eu115.gitpod.io',    
+ALLOWED_HOSTS = [
+    '8000-milosson-travelblog-5vyjtvxthhd.ws-eu115.gitpod.io',
     '.herokuapp.com'
 ]
 
@@ -40,8 +41,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.codeinstitute-ide.net',
     'https://*.herokuapp.com'
 ]
-# Application definition
 
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -52,7 +53,6 @@ INSTALLED_APPS = [
     'blog',
     'django_summernote',
     'about',
-
 ]
 
 MIDDLEWARE = [
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'mystar.urls'
@@ -87,26 +86,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mystar.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
+# Database configuration
 DATABASES = {
-    'default' : dj_database_url.parse("postgres://u3mpwxwhyjd:RekaesP3ADEo@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/steam_prism_ranch_984061")
-
+    'default': dj_database_url.parse("postgres://u3mpwxwhyjd:RekaesP3ADEo@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/steam_prism_ranch_984061")
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -122,28 +107,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-STATICFIELDS_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'  # Note the leading slash for URL
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]  # Adjusted to match your folder name
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_root')  # You can change this to another name if needed
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
