82864fb4e6db9f50440df352e03857d2fc4a969d
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-22 19:06:52 +0000

    Added comment display and count in post_detail view and template; updated settings and admin comments management

--- mystar/settings.py
+++ mystar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-8bh8)3pu@2f_v3n=+3a6n(h0w1xn5*$r^b@7esf)kc%o7z)v%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-milosson-travelblog-5vyjtvxthhd.ws-eu115.gitpod.io',
