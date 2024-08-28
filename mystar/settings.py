f3b9affa3cec760b03cb87752d25ea62086a57f3
Milosson Razzazzon <miladrazzaz@gmail.com>
2024-08-28 05:46:41 +0000

    Added cloudinary to requirements.txt

--- mystar/settings.py
+++ mystar/settings.py
@@ -26,10 +26,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-8bh8)3pu@2f_v3n=+3a6n(h0w1xn5*$r^b@7esf)kc%o7z)v%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-milosson-travelblog-mxfczvh5hxq.ws-eu115.gitpod.io',
